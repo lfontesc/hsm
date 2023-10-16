759cc505a2c0100ed36a4dbcc1079d769264e408
Lucas Fontes <lfc@cesar.org.br>
2023-10-16 15:26:23 -0300

    fix build

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const chronos = require('./historyScrapper.js');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
 
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 const mockItensAnalyse = [
   { id: 6635, name: "Bênção do Ferreiro", preco: 395 },
   { id: 38072, name: "[Extreme] Cristal de Bio 5",  preco: 12}
 ]
 
-function main() {
-  // console.log(mockItensAnalyse)
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta de conexão ${port}.`)
+})
 
-  return Promise.all(
+app.get('/itens', (request, response) => {
+    return Promise.all(
     mockItensAnalyse.map((item) => {
       return chronos(item)
       
     })).then(function (itens) {
       console.log(itens)
+      response.json({ itens: itens, app: "Olá mundo" })
     })
-}
+  
+})
 
-main()
+
+// function main() {
+//   // console.log(mockItensAnalyse)
+
+//   return Promise.all(
+//     mockItensAnalyse.map((item) => {
+//       return chronos(item)
+      
+//     })).then(function (itens) {
+//       console.log(itens)
+//     })
+// }
+
+// main()
